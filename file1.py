e15e078479d642194caa7b1539ea5d83179cffdd
Wachira Duangdee <wcrcorp@gmail.com>
2021-06-02 02:24:57 +0700

    Update file1.py

--- file1.py
+++ file1.py
@@ -2,3 +2,47 @@ with open("test.txt",'w',encoding = 'utf-8') as f:
    f.write("my first file\n")
    f.write("This file\n\n")
    f.write("contains three lines\n")
+#------------
+try:
+   f = open("test.txt", encoding = 'utf-8')
+   # perform file operations
+finally:
+   f.close()
+#-----------
+with open("test.txt", encoding = 'utf-8') as f:
+   # perform file operations
+#----------- 
+f = open("test.txt")      # equivalent to 'r' or 'rt'
+f = open("test.txt",'w')  # write in text mode
+f = open("img.bmp",'r+b') # read and write in binary mode
+#-----------
+>>> f = open("test.txt")    # open file in current directory
+>>> f = open("C:/Python38/README.txt")  # specifying full path
+#-----------
+f = open("test.txt", mode='r', encoding='utf-8')
+#-----------
+'''
+>>> f = open("test.txt",'r',encoding = 'utf-8')
+>>> f.read(4)    # read the first 4 data
+'This'
+
+>>> f.read(4)    # read the next 4 data
+' is '
+
+>>> f.read()     # read in the rest till end of file
+'my first file\nThis file\ncontains three lines\n'
+
+>>> f.read()  # further reading returns empty sting
+''
+'''
+#-----------
+'''
+Mode	Description
+r	Opens a file for reading. (default)
+w	Opens a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+x	Opens a file for exclusive creation. If the file already exists, the operation fails.
+a	Opens a file for appending at the end of the file without truncating it. Creates a new file if it does not exist.
+t	Opens in text mode. (default)
+b	Opens in binary mode.
++	Opens a file for updating (reading and writing)
+'''
