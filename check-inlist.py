71849c5a5fbe920fb25b6e5df85ac7124bdba1bd
Wachira Duangdee <wcrcorp@gmail.com>
2021-06-02 02:05:06 +0700

    Create check-inlist.py

---
+++ check-inlist.py
@@ -0,0 +1,8 @@
+#!python
+
+listA = ['Stranger Things', 'S samba', 'Game of Thrones']
+
+if ('S samba' in listA):
+    print("Yes, 'S Eductation' found in List : ", listA)
+else:
+    print("Not found")
