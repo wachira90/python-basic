1b6213ac9978fb6544625eca77746bc4c5d81ee3
Wachira Duangdee <wcrcorp@gmail.com>
2021-06-02 02:10:41 +0700

    Create datetime-example.py

---
+++ datetime-example.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+now = datetime.now() # current date and time
+
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)
+
+'''
+
+year: 2018
+month: 12
+day: 24
+time: 04:59:31
+date and time: 12/24/2018, 04:59:31
+'''
